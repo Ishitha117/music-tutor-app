8ede4320539c3c11462f37e7b48fc624a37e1d15
Ishitha <ishithacse26@gmail.com>
2026-02-23 23:52:56 +0530

    Updated API URL with live Render link

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // frontend/src/api.js
 
 // REPLACE THIS with the URL you got from Render in Phase 1
-const API_BASE_URL = "https://music-tutor-app.onrender.com"; 
+const API_BASE_URL = "https://music-tutor-app-r8wu.onrender.com"
 
 // Remove trailing slash if present to prevent double slashes
 const cleanBaseUrl = API_BASE_URL.replace(/\/$/, "");
